19465f5a78cfa953f82ab3941fdb60056e591c5f
kevin010 <kevin0312@qq.com>
2013-02-01 08:14:15 +0800

    osc 缓存

---
+++ src/test/my/cache/CacheException.java
@@ -0,0 +1,21 @@
+package my.cache;
+
+/**
+ * 缓存异常
+ * @author Winter Lau
+ */
+public class CacheException extends RuntimeException {
+
+	public CacheException(String s) {
+		super(s);
+	}
+
+	public CacheException(String s, Throwable e) {
+		super(s, e);
+	}
+
+	public CacheException(Throwable e) {
+		super(e);
+	}
+	
+}
